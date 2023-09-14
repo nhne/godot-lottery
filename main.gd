5532c23f4d386efe0452183b97724f9c78bb6c63
Kim Doyoung <disoxc21@gmail.com>
2023-09-15 00:34:53 +0900

    Make initial lottery

---
+++ main.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+
+@export var ball_scene: PackedScene
+
+var elapsed = 0
+var spin_speed = PI / 4
+var ball_numbers = []
+
+var angular_speed = PI * 2
+var speed = 200
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	for i in range(1, 46):
+		ball_numbers.append(i)
+	ball_numbers.shuffle()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if len(ball_numbers) > 0:
+		elapsed += delta
+		if elapsed > 0.06:
+			elapsed -= 0.06
+			_spawn_ball(ball_numbers.pop_front())
+	else:
+		# box
+		rotation -= spin_speed * delta
+
+		# pusher
+		var pusher = $Pusher
+		pusher.rotation += angular_speed * delta
+		var velocity = Vector2.UP.rotated(pusher.rotation) * speed
+		pusher.position += velocity * delta
+
+
+func _spawn_ball(number):
+	var ball = ball_scene.instantiate()
+	var ball_spawn = get_node("SpawnLocation")
+	ball.position = ball_spawn.position
+	ball.position += Vector2(randf_range(-5, 5), 0)
+	ball.set_label(number)
+	add_child(ball)
