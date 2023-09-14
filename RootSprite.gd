1a1bd215f8a3b85a17c6f81b27d8c20b7cf74a9b
Kim Doyoung <disoxc21@gmail.com>
2023-09-14 23:03:04 +0900

    Implement spinning 2d sprite

---
+++ RootSprite.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+
+var speed = 400
+var angular_speed = PI
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
